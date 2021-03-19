73fdb9a72ab2f0a4e5ba774e76af31f9d3cfe64c
root <tc.infomatics@gmail.com>
2021-03-19 04:38:34 +0000

    Autobuild

---
+++ r_course/presentations/r_code/Session2.R
@@ -0,0 +1,7 @@
+params <-
+list(isSlides = "no")
+
+## ----include=FALSE------------------------------------------------------------
+suppressPackageStartupMessages(require(knitr))
+knitr::opts_chunk$set(echo = TRUE, tidy = T)
+
